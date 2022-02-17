3011f91b34cd6b19ba2483ab9759c3923a36aef2
Nikita Kanushin <kanushinn@gmail.com>
2022-02-18 00:48:12 +0600

    added FSM

--- bot_telegram.py
+++ bot_telegram.py
@@ -8,7 +8,7 @@ async def on_startup(_):
 from handlers import trainee, admin, other
 
 trainee.register_handlers_trainee(dp)
-# admin.register_handlers_admin(dp)
+admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
